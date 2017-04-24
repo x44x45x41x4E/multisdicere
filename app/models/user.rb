fd6676465fcccfd5f833f7300ed6e1ed515bde06
Dean Canlas <d.canlas07@gmail.com>
2017-04-24 21:23:57 +0800

    Adds User model

---
+++ app/models/user.rb
@@ -0,0 +1,31 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
+
+# == Schema Information
+#
+# Table name: users
+#
+#  created_at             :datetime         not null
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :inet
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  first_name             :string
+#  id                     :integer          not null, primary key
+#  image                  :text
+#  last_name              :string
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :inet
+#  provider               :string
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  sign_in_count          :integer          default(0), not null
+#  uid                    :string
+#  updated_at             :datetime         not null
+#  username               :string
+#
