6dbcd7bfc50d5166cf71b59a86482eb2b8838fd7
Dean Canlas <d.canlas07@gmail.com>
2017-04-24 23:19:00 +0800

    Adds root page and simple Facebook sign up/sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  unauthenticated :user do
+  devise_scope :user do
+    root to: 'devise/sessions#new', as: 'unauthenticated_root'
+    end
+  end
+
+  authenticated :user do
+    root to: 'home#index'
+  end
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
