5a7e04fd9a6b046b7d8026b8ba4e81fc52206a09
Dean Canlas <d.canlas07@gmail.com>
2017-04-24 23:38:42 +0800

    Adds Membership model

---
+++ app/models/membership.rb
@@ -0,0 +1,20 @@
+class Membership < ApplicationRecord
+  belongs_to :user
+  belongs_to :joinable, polymorphic: true
+
+  scope :channels, -> { where(joinable_type: 'Channel') }
+  scope :teams, -> { where(joinable_type: 'Team') }
+  scope :conversations, -> { where(joinable_type: 'Conversation') }
+end
+
+# == Schema Information
+#
+# Table name: memberships
+#
+#  created_at    :datetime         not null
+#  id            :integer          not null, primary key
+#  joinable_id   :integer
+#  joinable_type :string
+#  updated_at    :datetime         not null
+#  user_id       :integer
+#
